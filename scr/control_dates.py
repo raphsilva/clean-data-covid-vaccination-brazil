5bbb4fa3f10a6dc4f560f8062be63df2f21e9f07
raphsilva <raphaelrs55@hotmail.com>
2021-04-24 20:38:46 -0400

    Register dates.

---
+++ scr/control_dates.py
@@ -0,0 +1,25 @@
+import os
+import json
+from datetime import datetime
+
+FILEPATH = 'register_dates.json'
+
+
+def read_register():
+    return json.loads(open(FILEPATH).read())
+
+
+def get_registered_time(uf):
+    r = read_register()[uf]
+    return datetime.strptime(r, '%Y-%M-%d %h:%m:%s')
+
+
+def register_time(uf, time):
+    time = str(time)
+    if os.path.isfile(FILEPATH):
+        register = read_register()
+    else:
+        register = dict()
+    register[uf] = time
+    f = open(FILEPATH, 'w')
+    f.write(json.dumps(register))
--- scr/main.py
+++ scr/main.py
@@ -1,5 +1,22 @@
 from get_data import get_data
+from datetime import datetime, timedelta, date
 
-d = get_data('SP', '2', '2021-04-22')
+from control_dates import get_registered_time, register_time
 
-print(d)
+uf = 'SP'
+date = '2021-04-22'
+
+date_A = datetime.strptime(date, '%Y-%M-%d')
+date_B = date_A + timedelta(days=7)
+
+d1 = get_data(uf, '1', date)
+d1['dose'] = '1'
+d2 = get_data(uf, '2', date)
+d2['dose'] = '2'
+
+data = d1.append(d2)
+data = data[['dose', 'count']]
+
+register_time(uf, date_B)
+
+print(data)
