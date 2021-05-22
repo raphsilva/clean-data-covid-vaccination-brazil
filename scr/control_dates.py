7feb1edde984b188977cc477d61d1354f3047a31
raphsilva <raphaelrs55@hotmail.com>
2021-05-22 17:32:31 -0400

    Fixed directory access to previous data.

--- scr/control_dates.py
+++ scr/control_dates.py
@@ -6,7 +6,7 @@ from time_format import date_to_timestamp
 
 
 def get_last_time(uf):
-    path = get_directory_path(uf)
+    path = get_directory_path(uf, subfolder='complete')
     if not os.path.isdir(path) or len(os.listdir(path)) == 0:
         return date_to_timestamp(MIN_DATE)
     last_file = sorted([f for f in os.listdir(path) if os.path.isfile(f'{path}/{f}')])[-1]
